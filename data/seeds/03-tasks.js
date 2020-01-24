d8fe97f847d93e2f28a317230b9532e3c2c7e099
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-01-24 12:47:09 -0500

    added seeds, now to run seeds

---
+++ data/seeds/03-tasks.js
@@ -0,0 +1,17 @@
+
+exports.seed = function(knex) {
+    return knex('resources').insert([
+    {
+        task_description: 'open vs code, make react app, add redux, authorization key',
+        notes: 'take a break now and then from coding',
+        project_id: 1
+    },
+
+    {
+        task_description: 'dust dresser and desk, vacuum floor',
+        notes: ' dirty dirty dirty',
+        project_id: 2
+    }
+
+    ]);
+}
